8d5eb9fd0e4083fc3ca3eef3a0b1841103b9332b
Gustavo Giordano <101727973+Giordano1901@users.noreply.github.com>
2022-10-14 13:05:12 -0300

    Add files via upload

--- 13 10/header.php
+++ 13 10/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
     <title>E.E. Pedro Alvares Cabral</title>
 </head>
@@ -12,10 +13,10 @@
         <img src="https://media.discordapp.net/attachments/595706481555537969/1030091282678296617/unknown.png" alt="">
         <h2>E.E. Pedro Álvares Cabral</h2>
         <ul>
-            <li><a href="">SESCRETARIA</a></li>
-            <li><a href="">BOLETIM</a></li>
-            <li><a href="">ALUNO</a></li>
-            <li><a href="">MATRICULA</a></li>
-            <li><a href="">MINHAS FÉRIAS</a></li>
+            <a href=""><li>SECRETARIA</li></a>
+            <a href=""><li>BOLETIM</li></a>
+            <a href=""><li>ALUNO</li></a>
+            <a href=""><li>MATRICULA</li></a>
+            <a href=""><li>MINHAS FÉRIAS</li></a>
         </ul>
     </header>
--- 13 10/index.php
+++ 13 10/index.php
@@ -2,23 +2,14 @@
     include 'header.php';
 ?>
 
-<main>
-    <div>
-        <h1>Como foi as minhas férias</h1>
-    </div>
-    <div>
+<main id="MinhasFerias">
+    <img src="https://media.discordapp.net/attachments/595706481555537969/1030501482488922163/unknown.png" alt="">
+    <div id="FormMinhasFerias">
         <form action="" method="get">
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-            <h3>Ou você pode adicionar uma imagem de sua atividade:</h3>
-            <div>
-                <input type="file" name="imagem" id="img-input">
-            </div>
-            <div id="img-container">
-                <img id="preview" src="">
-            </div>
-            <div>
-                <input type="submit" value="Enviar">
-            </div>
+            <textarea name="" id="" cols="140" rows="30" placeholder="Escreva aqui como foi suas férias..."></textarea>
+            <h1>Ou você pode adicionar uma imagem de sua atividade:</h1>
+            <input type="file" name="imagem" id="img-input">
+            <input type="submit" value="Enviar">
         </form>
     </div>
 </main>
