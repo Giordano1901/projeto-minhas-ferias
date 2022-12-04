b131d792f1a95fa3f8550a8f7994af331d261848
Gustavo Giordano <101727973+Giordano1901@users.noreply.github.com>
2022-12-04 18:02:28 -0300

    Delete 31 10 directory

--- 31 10/script.js
+++
@@ -1,7 +0,0 @@
-const btnMobile = document.getElementById('btn-mobile');
-const ulMenu = document.querySelector('#menu');
-var btn_close = document.querySelector('.close');
-
-btnMobile.addEventListener('click', function(){
-    ulMenu.classList.toggle('close');
-});
